7751b918dae6f74f7d600ab0b4d90287110eeeaf
Carolina Tacconis <carolinatacconis@MacBook-Pro-de-Carolina.local>
2024-04-29 17:45:23 +0200

    ft_rrange

---
+++ ft_rrange.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rrange.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carolinatacconis <marvin@42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 17:44:54 by carolinat         #+#    #+#             */
+/*   Updated: 2024/04/29 17:45:04 by carolinat        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_rrange(int start, int end)
+{
+	int	*array;
+	int	i;
+	int	len;
+
+	if (start > end)
+	{
+		len = start - end + 1;
+		array = malloc(sizeof(int) * len);
+		while (i < len)
+		{
+			array[i] = start;
+			start--;
+			i++;
+		}
+	}
+	else
+	{
+		len = end - start + 1;
+		array = malloc(sizeof(int) * len);
+		while (i < len)
+		{
+			array[i] = end;
+			end--;
+			i++;
+		}
+	}
+	return (array);
+}
