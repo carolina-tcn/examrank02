e2e294d6e0b3ed2140283b0689be8cf7e0aba443
Carolina <159081669+carolina-tcn@users.noreply.github.com>
2024-05-02 18:48:49 +0200

    Update rstr_capitalizer.c

--- rstr_capitalizer.c
+++ rstr_capitalizer.c
@@ -31,8 +31,10 @@ int	main(int argc, char **argv)
 			if (argv[i][j] >= 'A' && argv[i][j] <= 'Z')
 				argv[i][j] += 32;
 			if ((argv[i][j + 1] == ' ' || argv[i][j + 1] == '\t' || argv[i][j + 1] == '\0') && (argv[i][j] >= 'a' && argv[i][j] <= 'z'))
+			{
 				argv[i][j] -= 32;
 				write(1, &argv[i][j], 1);
+			}
 			j++;
 		}
 		write(1, "\n", 1);
