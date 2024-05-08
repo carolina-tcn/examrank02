669ba7b2e147e898d0d5168235baf03722955410
Carolina <159081669+carolina-tcn@users.noreply.github.com>
2024-05-08 22:16:05 +0200

    Update ft_strpbrk.c

--- ft_strpbrk.c
+++ ft_strpbrk.c
@@ -13,10 +13,11 @@
 //function locates the first occurrence in the string s of any of the bytes in the string accept
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
-	int	i = 0;
+	int	i;
 	
 	while (*s1)
 	{
+		i = 0;
 		while (s2[i])
 		{
 			if (*s1 == s2[i])
