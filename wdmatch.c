fe22849b63328a109e185ba35f4f647bbf979b19
Carolina <159081669+carolina-tcn@users.noreply.github.com>
2024-06-18 20:19:58 +0200

    Update wdmatch.c

--- wdmatch.c
+++ wdmatch.c
@@ -12,25 +12,25 @@
 
 #include <unistd.h>
 
+void	wdmatch(char *s1, char *s2)
+{
+	int	i = 0;
+	int	j = 0;
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		if (s1[i] == s2[j])
+			i++;
+		j++;
+	}
+	if (s1[i] == '\0')
+		write(1, s1, i);
+}
+
 int	main(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
 	if (argc == 3)
-	{
-		while (argv[1][i])
-		{
-			while (*argv[2] != argv[1][i] && *argv[2] != '\0')
-			{
-				argv[2]++;
-				if (*argv[2] == '\0')
-					break ;
-			}
-			i++;
-		}
-		write(1, argv[1], i);
-	}
+		wdmatch(argv[1], argv[2]);
 	write(1, "\n", 1);
 	return (0);
 }
