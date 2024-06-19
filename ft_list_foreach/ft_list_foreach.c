4fe20125e14a43198fed0948528236edaffee5d3
Carolina <159081669+carolina-tcn@users.noreply.github.com>
2024-06-19 19:45:58 +0200

    Update ft_list_foreach.c

--- ft_list_foreach/ft_list_foreach.c
+++ ft_list_foreach/ft_list_foreach.c
@@ -17,7 +17,7 @@ void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 	t_list *list_ptr;
 	list_ptr = begin_list;
 
-	while (begin_list)
+	while (list_ptr)
 	{
 		(*f)(list_ptr->data);
 		list_ptr = list_ptr->next;
